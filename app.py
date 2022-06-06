9dbd66136e6e971d61138de9d912fc3c39523b8f
jorella1 <johnorellano@gmail.com>
2022-06-06 16:35:16 -0400

    add bootstrap basics

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, request, url_for, redirect, abort
+from flask_bootstrap import Bootstrap
+from nav import nav
+from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
 
 
 app = Flask(__name__)
+Bootstrap(app)
+nav.init_app(app)
+
+nav.register_element('frontend_top', Navbar(
+    View('Project 1', '.index'),
+    View('Home', '.index'),
+    View('Login', '.login_page'),
+    ))
+
 
 
 @app.route('/', methods=["GET"])
@@ -9,6 +21,9 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/login', methods=["GET"])
+def login_page():
+    pass
 
 if __name__== "__main__":
     app.run()
